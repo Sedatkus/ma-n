91c16f02dd3a94c1f5d930832d469ec782db2a3d
Sedatkus <118366314+Sedatkus@users.noreply.github.com>
2022-11-16 21:11:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Merhaba Etiya")
+text = "10"
+print(text)
+number = 5
+print(number)
+dnumber = 5.3
+print(dnumber)
+print(number + 5)
+print(number - 5)
+print(number * 5)
+print(number / 5)
+print(number % 3)
+print(1 == 2)
+print(2 != 2)
+text = "Merhaba Etiya"
+#print(text.upper())
+print(text.lower())
+name = "Halit"
+age= "23"
+company = "Kodlamaio"
+print(f"{name} {age} yaşında {company}'da çalışıyor")
+print(name + " " + age + " " + "yaşında" + " " + company + "'da çalışıyor")
