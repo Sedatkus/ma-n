bd3513356f8516a20d3131945eaae78f5df3112c
Sedatkus <118366314+Sedatkus@users.noreply.github.com>
2022-11-16 21:28:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 print("Merhaba Etiya")
+#! string = metinsel ifade
 text = "10"
 print(text)
+#! integer = tam sayı
 number = 5
 print(number)
+#! double,float,decimal = ondalıklı sayı
 dnumber = 5.3
+#! matematiksel
 print(dnumber)
 print(number + 5)
 print(number - 5)
 print(number * 5)
 print(number / 5)
 print(number % 3)
+#! boolean,bool = true veya false
 print(1 == 2)
 print(2 != 2)
 text = "Merhaba Etiya"
@@ -20,3 +25,4 @@ age= "23"
 company = "Kodlamaio"
 print(f"{name} {age} yaşında {company}'da çalışıyor")
 print(name + " " + age + " " + "yaşında" + " " + company + "'da çalışıyor")
+print("İlk Gün Ödevi SEDAT KUŞ")
