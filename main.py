b0730447564fcdde53cb97ae45a6baf5ca6b03cc
Sedatkus <118366314+Sedatkus@users.noreply.github.com>
2022-11-17 00:46:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,18 +8,31 @@ print(number)
 #! double,float,decimal = ondalıklı sayı
 dnumber = 5.3
 #! matematiksel
-print(dnumber)
-print(number + 5)
-print(number - 5)
-print(number * 5)
-print(number / 5)
-print(number % 3)
+print(dnumber) # ondalıklı ifadeyi yazdırır
+print(number + 5) # 5 + 5 = 10
+print(number - 5) # 5 - 5 = 0
+print(number * 5) # 5 * 5 = 25
+print(number / 5) # 5 / 5 = 1
+print(number % 3) # Bu ifade kalanı verir = 2
 #! boolean,bool = true veya false
-print(1 == 2)
-print(2 != 2)
+print(1 == 1) # True
+print(2 != 2) # False
+
+print(1 > 2) # F
+print(1 < 2) # T
+
+print(1 <= 2) # T
+print(1 >= 2) # F
+
+print(10 % 2 == 0) # T
+print(50 / 2 == 25) # T
+print(50 / 2 == 25.0) # T
+
 text = "Merhaba Etiya"
-#print(text.upper())
-print(text.lower())
+#print(text.upper()) # Büyük harfle yazar
+print(text.lower()) # Küçük harfle yazdırır
+print(text.startswith("Mer"))
+print(text.endswith("Etiya"))
 name = "Halit"
 age= "23"
 company = "Kodlamaio"
